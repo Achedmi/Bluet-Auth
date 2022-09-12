34fb8661ce1948d08a97dcea349fd4d847220359
Abdelkarim Chedmi <achedmi@e1r13p3.1337.ma>
2022-09-12 20:31:06 +0100

    running script with launchctl

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BluetoothHandler
+put "boot-shutdown-script.plist" in ~/Library/LaunchAgents
+use launchctl load ~/Library/LaunchAgents/boot-shutdown-script.plist to load it
+use launchctl unload ~/Library/LaunchAgents/boot-shutdown-script.plist to unload it
--- bluetoothHandler.sh
+++ bluetoothHandler.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-count=$(ps | grep "sleep 86400" | grep -v grep | wc -l)
-
-if [ $count -lt 1 ]; then
-    onLogout() {
-        for address in $($(dirname ${BASH_SOURCE[0]})/blueutil --paired | awk '{print $2}' | cut -d "," -f 1)
-        do
+onLogout() {
+    for address in $($(dirname ${BASH_SOURCE[0]})/blueutil --paired | awk '{print $2}' | cut -d "," -f 1)
+    do
         $(dirname ${BASH_SOURCE[0]})/blueutil --unpair "$address"
-        done
-        exit
-    }
-    trap 'onLogout' SIGINT SIGHUP SIGTERM SIGKILL
-    while true; do
-        sleep 86400
     done
-fi
-clear
+    exit
+}
+
+trap 'onLogout' SIGINT SIGHUP SIGTERM SIGKILL 
+
+while true; do
+    sleep 86400 &
+    wait $!
+done
